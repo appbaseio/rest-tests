877bcf0f676ab84aa7f59b94521d1dba7c3711f2
Sagar <sagar.chandarana123@gmail.com>
2015-02-21 11:45:53 -0600

    more tests, reademe.md

--- test_data.js
+++ test_data.js
@@ -1,37 +1,160 @@
 module.exports = {
   global : {
-    base: "http://api.appbase.io/rest_test/v2",
+    base: "http://api.appbase.io/rest_test/v3",
     req: {
-      headers: {
+      headers: { // request headers
         "appbase-secret": "193dc4d2440146082ea734f36f4f2638",
         "content-type": "application/json"
       }
     },
     res: {
-      headers: {
+      headers: {  //expected response
         "content-type": "application/json"
       }
     }
   },
   tests: [
     {
-      description: "Fetch Document",
-      req: {
-        url: "/Materials/Ice/~properties",
-        method: "post",
+      description: "Create/Update Document",
+      req: { // request to perform
+        url: "/user/laura",
+        method: "patch",
+        "foo": "bar"
+      },
+      res: { //expected response
+        code: 200,
         body: {
-          all: true
+          "_id": "laura", // has to match the exact string
+          "_collection": "user",
+          "_timestamp": Number, // could be any number
+          "foo": "bar"
+        }
+      }
+    },
+    {
+      description: "Patch Nested Document",
+      req: {
+        url: "/user/laura",
+        method: "patch",
+        body: {
+          "foo.bar": "bar"
         }
       },
       res: {
         code: 200,
         body: {
-          "vertex": {
-            "_id": String,
-            "timestamp": Number,
-            "rootPath": "Materials/Ice"
-          },
-          "optype": "RETR"
+          "_id": "laura",
+          "_collection": "user",
+          "_timestamp": Number,
+          "foo": {
+            "bar": "bar"
+          }
+        }
+      }
+    },
+    {
+      description: "Delete Properties, add new ones",
+      req: {
+        url: "/user/laura",
+        method: "patch",
+        body: {
+          "foo": null,
+          "bar": "foo"
+        }
+      },
+      res: {
+        code: 200,
+        body: {
+          "_id": "laura",
+          "_collection": "user",
+          "_timestamp": Number,
+          "bar": "foo"
+        }
+      }
+    },
+    {
+      description: "Get Document",
+      req: {
+        url: "/user/laura",
+        method: "get"
+      },
+      res: {
+        code: 200,
+        body: {
+          "_id": "laura",
+          "_collection": "user",
+          "_timestamp": Number,
+          "bar": "foo"
+        }
+      }
+    },
+    {
+      description: "Delete Document",
+      req: {
+        url: "/user/laura",
+        method: "delete"
+      },
+      res: {
+        code: 200,
+        body: {
+          "_id": "laura",
+          "_collection": "user",
+          "_timestamp": Number,
+          "_deleted": true,
+          "bar": "foo"
+        }
+      }
+    },
+    {
+      description: "Fetching Non-existent document",
+      req: {
+        url: "/user/laura",
+        method: "delete"
+      },
+      res: {
+        code: 404,
+        body: {
+          error: Number,
+          message: String
+        }
+      }
+    },
+    {
+      description: "Push a JSON without an _id in the collection",
+      req: {
+        url: "/user",
+        method: "post",
+        body: {
+          "bar": "foo"
+        }
+      },
+      res: {
+        code: 200,
+        body: {
+          "_id": String,
+          "_collection": "user",
+          "_timestamp": Number,
+          "bar": "foo"
+        }
+      }
+    },
+    {
+      description: "Push a JSON with an _id in the collection ",
+      req: {
+        url: "/user",
+        method: "post",
+        body: {
+          "_id": "laura",
+          "bar": "foo"
+        }
+      },
+      res: {
+        code: 200,
+        body: {
+          "_id": "laura",
+          "_collection": "user",
+          "_timestamp": Number,
+          "bar": "foo"
         }
       }
     }
