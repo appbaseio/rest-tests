62efd300775934054c9a10d050b35f89760fa76e
Sagar <sagar@appbase.io>
2015-02-27 21:05:24 -0600

    more tests on references

--- test_data.js
+++ test_data.js
@@ -74,12 +74,30 @@ module.exports = {
       }
     },
     {
-      description: "Delete Properties, add new ones",
+      description: "Delete Properties",
       req: {
         url: "/user/laura",
         method: "patch",
         body: {
-          "foo": null,
+          "foo": null
+        }
+      },
+      res: {
+        code: 200,
+        body: {
+          "_id": "laura",
+          "_collection": "user",
+          "_timestamp": Number,
+          "foo": null // only in this request, it won't appear in a get request
+        }
+      }
+    },
+    {
+      description: "Add new properties",
+      req: {
+        url: "/user/laura",
+        method: "patch",
+        body: {
           "bar": "foo"
         }
       },
@@ -106,66 +124,6 @@ module.exports = {
           "_collection": "user",
           "_timestamp": Number,
           "bar": "foo"
-        }
-      }
-    },
-    {
-      description: "Delete Document",
-      req: {
-        url: "/user/laura",
-        method: "delete"
-      },
-      res: {
-        code: 200,
-        body: {
-          "_id": "laura",
-          "_collection": "user",
-          "_timestamp": Number,
-          "_deleted": true,
-          "bar": "foo"
-        }
-      }
-    },
-    {
-      description: "Operating on a Non-existent document",
-      req: {
-        url: "/user/laura",
-        method: "delete"
-      },
-      res: {
-        code: 404,
-        body: {
-          //error: Number,
-          message: String
-        }
-      }
-    },
-    {
-      description: "Delete a collection",
-      req: {
-        url: "/user",
-        method: "delete"
-      },
-      res: {
-        code: 200,
-        body: {
-          "_collection": "user",
-          "_deleted": true,
-          "_createdAt": Number
-        }
-      }
-    },
-    {
-      description: "Create a new collection",
-      req: {
-        url: "/user",
-        method: "delete"
-      },
-      res: {
-        code: 200,
-        body: {
-          "_collection": "user",
-          "_deleted": true
         }
       }
     },
@@ -252,6 +210,184 @@ module.exports = {
       }
     },
     {
+      description: "Create reference",
+      req: {
+        url: "/user/laura",
+        method: "patch",
+        body: {
+          "/tweet": "tweet/tweet1"
+        }
+      },
+      res: {
+        code: 200, 
+        body: {
+          "_id": "laura",
+          "_collection": "user",
+          "_timestamp": Number,
+          "/tweet": {
+            "_id": "tweet1",
+            "_collection": "tweet",
+            "_timestamp": Number,
+            "bar": "foo"
+          }
+        }
+      }
+    },
+    {
+      description: "Create reference to a non existing path",
+      req: {
+        url: "/user/laura",
+        method: "patch",
+        body: {
+          "/tweet2": "tweet/tweet2"
+        }
+      },
+      res: {
+        code: 400, 
+        body: {
+          //error: Number,
+          message: String
+        }
+      }
+    },
+    {
+      description: "Multiple reference provied in a single request",
+      req: {
+        url: "/user/laura",
+        method: "patch",
+        body: {
+          "/tweet1": "tweet/tweet1",
+          "/tweet2": "tweet/tweet2"
+        }
+      },
+      res: {
+        code: 400, 
+        body: {
+          //error: Number,
+          message: String
+        }
+      }
+    },
+    {
+      description: "References and properties provided together",
+      req: {
+        url: "/user/laura",
+        method: "patch",
+        body: {
+          "dances": "forro",
+          "/tweet2": "tweet/tweet2"
+        }
+      },
+      res: {
+        code: 400, 
+        body: {
+          //error: Number,
+          message: String
+        }
+      }
+    },
+    {
+      description: "Get a deeper document",
+      req: {
+        url: "/user/laura/tweet",
+        method: "get"
+      },
+      res: {
+        code: 200,
+        body: {
+          "_id": "tweet1",
+          "_collection": "tweet",
+          "_timestamp": Number,
+          "bar": "foo"
+        }
+      }
+    },
+    {
+      description: "Get a deeper non existing document",
+      req: {
+        url: "/user/laura/tweet2/tweefafv",
+        method: "get"
+      },
+      res: {
+        code: 400,
+        body: {
+          //error: Number,
+          message: String
+        }
+      }
+    },
+    {
+      description: "Get the document with references",
+      req: {
+        url: "/user/laura",
+        method: "get"
+      },
+      res: {
+        code: 200,
+        body: {
+          "_id": "laura",
+          "_collection": "user",
+          "_timestamp": Number,
+          "bar": "foo",
+          "/tweet": {
+            "_id": "tweet1",
+            "_collection": "tweet",
+            "_timestamp": Number,
+            "bar": "foo"
+          }
+        }
+      }
+    },
+    {
+      description: "Remove a reference",
+      req: {
+        url: "/user/laura",
+        method: "patch",
+        body: {
+          "/tweet": null
+        }
+      },
+      res: {
+        code: 200, 
+        body: {
+          "_id": "laura",
+          "_collection": "user",
+          "_timestamp": Number,
+          "/tweet": null
+        }
+      }
+    },
+    {
+      description: "See if the reference actually got deleted",
+      req: {
+        url: "/user/laura",
+        method: "get"
+      },
+      res: {
+        code: 200,
+        body: {
+          "_id": "laura",
+          "_collection": "user",
+          "_timestamp": Number,
+          "bar": "foo"
+        }
+      }
+    },
+    {
+      description: "Trying to fetch a deleted path",
+      req: {
+        url: "/user/laura/tweet",
+        method: "get"
+      },
+      res: {
+        code: 400,
+        body: {
+          //error: Number,
+          message: String
+        }
+      }
+    },
+    {
       description: "Delete a collection",
       req: {
         url: "/tweet",
@@ -290,6 +426,70 @@ module.exports = {
         body: {
           //error: Number,
           message: String
+        }
+      }
+    },
+    {
+      description: "Delete Document",
+      req: {
+        url: "/user/laura",
+        method: "delete"
+      },
+      res: {
+        code: 200,
+        body: {
+          "_id": "laura",
+          "_collection": "user",
+          "_timestamp": Number,
+          "_deleted": true,
+          "bar": "foo"
+        }
+      }
+    },
+    {
+      description: "Operating on a Non-existent document",
+      req: {
+        url: "/user/laura",
+        method: "delete"
+      },
+      res: {
+        code: 404,
+        body: {
+          //error: Number,
+          message: String
+        }
+      }
+    },
+    {
+      description: "Delete a collection",
+      req: {
+        url: "/user",
+        method: "delete"
+      },
+      res: {
+        code: 200,
+        body: {
+          "_collection": "user",
+          "_deleted": true,
+          "_createdAt": Number
+        }
+      }
+    },
+    {
+      description: "Global endpoint: see if the namespaces actually got deleted",
+      req: {
+        url: "/",
+        method: "get"
+      },
+      res: {
+        code: 200,
+        body: {
+          "_time": {
+            "_timestamp": Number,
+            "_timezone": "UTC",
+            "_ISOString": String,
+          },
+          _collections: []
         }
       }
     }
